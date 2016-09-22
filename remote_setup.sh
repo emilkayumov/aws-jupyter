0a4b0d4188da919a956ca727f148e29d742574ea
emilkaumov <emil.kayumov@gmail.com>
2016-09-22 21:15:27 +0300

    Simplified the chapter 2.

--- remote_setup.sh
+++ remote_setup.sh
@@ -1,10 +1,8 @@
 sudo locale-gen ru_RU.UTF-8
 
-# miniconda with some libraries
-wget -c https://repo.continuum.io/miniconda/Miniconda3-4.0.5-Linux-x86_64.sh
-bash Miniconda3-4.0.5-Linux-x86_64.sh -b -p /home/ubuntu/miniconda
-export PATH=/home/ubuntu/miniconda/bin:$PATH
-conda install -y numpy scipy pandas scikit-learn jupyter matplotlib
+# install some libraries for standard python
+sudo apt-get -y iinstall nstall python-pip
+pip install numpy scipy pandas scikit-learn jupyter matplotlib
 
 
 # some useful things
@@ -25,20 +23,5 @@ sudo ../../miniconda/bin/python setup.py install
 cd ../..
 
 
-# install vw
+# install vowpal wabbit
 sudo apt-get -qq install vowpal-wabbit
-
-
-# create ipython profile
-ipython profile create nbserver
-
-printf "\n# Configuration file for ipython-notebook.\n
-c = get_config()\n
-# Notebook config\n
-c.NotebookApp.password = u'"$1"'\n
-c.NotebookApp.ip = '*'\n
-c.NotebookApp.open_browser = False\n
-c.NotebookApp.port = 8888\n" > ~/.ipython/profile_nbserver/ipython_notebook_config.py
-
-# open ipython (better from new connection)
-# jupyter notebook --config="~/.ipython/profile_nbserver/ipython_notebook_config.py" --certfile=jupyter.pem
